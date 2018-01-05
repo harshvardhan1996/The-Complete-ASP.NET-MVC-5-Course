f7cfa4798d9756422fd089b47150e4fd0e298c6b
Lee4Gitr <Lee4Gitr@gmail.com>
2018-01-04 22:52:03 -0500

    Broken

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -28,7 +28,8 @@ namespace Vidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
-                membershipTypes = membershipTypes
+                Customer = new Customer(),
+                MembershipTypes = membershipTypes
             };
 
             return View("CustomerForm", viewModel);
@@ -37,6 +38,16 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm");
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
@@ -80,7 +91,7 @@ namespace Vidly.Controllers
             var viewModel = new CustomerFormViewModel
             {
                 Customer = customer,
-                membershipTypes = _context.MembershipTypes.ToList()
+                MembershipTypes = _context.MembershipTypes.ToList()
             };
 
             return View("CustomerForm", viewModel);
